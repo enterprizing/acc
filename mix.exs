b14ff4b08b116fe3379119a2a39d2110f448d2ba
Namdak Tonpa <maxim@synrc.com>
2019-06-24 21:18:24 +0300

    desc

---
+++ mix.exs
@@ -0,0 +1,34 @@
+defmodule ACC.Mixfile do
+  use Mix.Project
+
+  def project() do
+    [
+      app: :acc,
+      version: "0.6.0",
+      elixir: "~> 1.7",
+      description: "ACC Account Management",
+      package: package(),
+      deps: deps()
+    ]
+  end
+
+  def package do
+    [
+      files: ~w(doc src mix.exs LICENSE),
+      licenses: ["ISC"],
+      maintainers: ["Namdak Tonpa"],
+      name: :acc,
+      links: %{"GitHub" => "https://github.com/enterprizing/acc"}
+    ]
+  end
+
+  def application() do
+    [mod: {:acc, []}]
+  end
+
+  def deps() do
+    [
+      {:ex_doc, "~> 0.11", only: :dev}
+    ]
+  end
+end
